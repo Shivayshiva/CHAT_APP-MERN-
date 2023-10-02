71b461d1fc416b96fe60ff8841ac2400cd894960
Shivayshiva <prashantmaddheshiya@gmail.com>
2023-10-02 20:57:03 +0530

    Changes in mongodb url and use of mongo atlas

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    //useCreateIndex:true,
+    //useFindAndModify:false
   })
   .then(() => {
     console.log("DB Connetion Successfull");
@@ -21,6 +23,8 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+
+  
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
